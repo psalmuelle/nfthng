fc3bbf59c36755d8db1e46747cd50d0ecd2897a0
psalmuelle <bjoestores@gmail.com>
2022-11-13 22:15:21 +0100

    setup file structure

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./home.css"
+
+
+export default function LandingPage (){
+    return(
+        <div>
+            <header>
+
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+                
+            </footer>
+        </div>
+    )
+}
